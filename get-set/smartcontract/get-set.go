ec77252abd7dc710b4f4cf465a8b3d0350f98e0b
Rajat Shamra <rajat.sharma@xooa.com>
2019-11-01 17:48:22 +0530

    readme and license information

--- get-set/smartcontract/get-set.go
+++ get-set/smartcontract/get-set.go
@@ -1,7 +1,7 @@
 /**
  *  Xooa get-set smart contract
  *
- *  Copyright 2018 Xooa
+ *  Copyright 2019 Xooa
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
