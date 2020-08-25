3a9022d08cce93ff07e090c50fb02a46effe050b
Sandeep Patel <37658717+sandeepxooa@users.noreply.github.com>
2020-08-25 15:09:08 +0530

    Update balance-transfer.js

--- balance-transfer-node/balance-transfer.js
+++ balance-transfer-node/balance-transfer.js
@@ -65,7 +65,7 @@ var Chaincode = class {
     let method = this[ret.fcn];
     if (!method) {
       console.log('no method of name:' + ret.fcn + ' found');
-      return shim.success();
+      return shim.error(new Error('no method of name:' + ret.fcn + ' found''));
     }
     try {
       let payload = await method(stub, ret.params);
